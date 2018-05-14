ef1ea991eb947da5885cbbcb54aed5aa9e458208
Nicolas Venegas Oliva <nvenegas.oliva@gmail.com>
2018-05-14 15:02:24 -0400

    Add hadoop training project

---
+++ hadoop/src/main/java/com/nvenegas/training/hadoop/App.java
@@ -0,0 +1,13 @@
+package com.nvenegas.training.hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
